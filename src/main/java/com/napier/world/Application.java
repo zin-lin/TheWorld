c12f60c63c3913194640637501591c3a23311508
zin-htun <zinlinhtun34@gmail.com>
2023-02-03 01:52:54 +0000

    New Release

--- src/main/java/com/napier/world/Application.java
+++ src/main/java/com/napier/world/Application.java
@@ -3,8 +3,6 @@ package com.napier.world;
 import com.napier.world.mongo.MongoIntegration;
 
 public class Application {
-
-
     public static  void main (String args []){
         System.out.println("Hello DevOps 13");
         MongoIntegration.integrate(); // Call to Build and View the Database
