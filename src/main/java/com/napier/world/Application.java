41392744b019bc57e58a4d02c0a5a82ad8bb737b
zin-htun <zinlinhtun34@gmail.com>
2023-02-10 17:31:11 +0000

    Added MYSQL

--- src/main/java/com/napier/world/Application.java
+++ src/main/java/com/napier/world/Application.java
@@ -1,10 +1,8 @@
 package com.napier.world;
 
-import com.napier.world.mongo.MongoIntegration;
-
 public class Application {
-    public static  void main (String args []){
+    public static  void main (String []args ){
         System.out.println("Hello DevOps 13");
-        MongoIntegration.integrate(); // Call to Build and View the Database
+        SqlConnection.connection(); // Call to Build and View the Database
     }
 }
