218d51bb196731f7fcea56e5fd712f9b6546b5e8
zin-htun <zinlinhtun34@gmail.com>
2023-02-13 11:51:58 +0000

    add

--- src/main/java/com/napier/world/Application.java
+++ src/main/java/com/napier/world/Application.java
@@ -3,6 +3,13 @@ package com.napier.world;
 public class Application {
     public static  void main (String []args ){
         System.out.println("Hello DevOps 13");
-        SqlConnection.connection(); // Call to Build and View the Database
+        SqlConnection sqlConnection = new SqlConnection(); // Call to Build and View the Database
+        sqlConnection.connect();
+        Employee emp = sqlConnection.getEmployee(255530);
+        // Display results
+        sqlConnection.displayEmployee(emp);
+
+        // Disconnect from database
+        sqlConnection.disconnect();
     }
 }
